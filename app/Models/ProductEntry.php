6fa3e802d5f31c7f42c2e4652abd2ddc2926e7b5
Prakhar Gyawali <38656630+PBGyawali@users.noreply.github.com>
2023-04-29 16:46:06 +0200

    new model added

---
+++ app/Models/ProductEntry.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductEntry extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['product_id','entry_date','total value','opening_stock'];
+}
